0220b3387f7e9982e815573aa86354eae7d36aff
carawaters <cara_waters@icloud.com>
2019-10-14 09:47:07 +0100

    add optical element class and spherical refraction child class make direction vector normalised

--- raytracer.py
+++ raytracer.py
@@ -9,17 +9,17 @@ import scipy as sp
 
 
 class Ray:
-"""
-Ray class gives a ray with a list of positions and direction vectors.
-Initialisation takes 3d lists for position and direction.
-"""
-
-    ray_ps = []
-    ray_ks = []
+    """
+    Ray class gives a ray with a list of positions and direction vectors.
+    Initialisation takes 3d lists for position and direction.
+    """
 
     def __init__(self, pos=[0.0, 0.0, 0.0], direc=[0.0, 0.0, 0.0]):
         self._r = sp.array(pos)
         self._d = sp.array(direc)
+        squares = self._d**2
+        mag = sp.sqrt(squares[0] + squares[1] + squares[2])
+        self._dhat = self._d/mag
         self.poslist = [sp.array(pos)]
         self.dirlist = [sp.array(direc)]
 
@@ -27,11 +27,36 @@ Initialisation takes 3d lists for position and direction.
         return self._r
 
     def k(self):
-        return self._d
+        return self._dhat
 
     def append(self, p, k):
         self.poslist.append(sp.array(p))
         self.dirlist.append(sp.array(k))
 
     def vertices(self):
-        return self.ray_ps
+        return self.poslist
+
+class OpticalElement:
+    """
+
+    """
+
+    def __init__(self):
+        # add attributes
+
+    def propagate_ray(self, ray):
+        "propagate a ray through the optical element"
+        raise NotImplementedError()
+
+class SphericalRefraction(OpticalElement):
+    """
+
+    """
+
+    def __init__(self, z_0, curve, n1, n2, aperture):
+        self.z_0 = z_0
+        self.curve = curve
+        self.n1 = n1
+        self.n2 = n2
+        self.aperture = aperture
+        OpticalElement.__init__(self)
