c4193080fdd938e0bf15fa4ee23e4bf4f3b8d8d0
carawaters <cara_waters@icloud.com>
2019-10-29 16:10:36 +0000

    add docstrings and comments

--- raytracer.py
+++ raytracer.py
@@ -7,6 +7,7 @@ Includes a class for an optical ray.
 
 import scipy as sp
 
+
 class Ray:
     """
     Gives a ray with a list of positions and direction vectors.
@@ -17,7 +18,7 @@ class Ray:
         self._r = sp.array(pos)
         self._d = sp.array(direc)
         mag_d = sp.sqrt(sp.dot(self._d, self._d))
-        self._dhat = self._d/mag_d
+        self._dhat = self._d / mag_d
         self.poslist = sp.array([sp.array(pos)])
         self.dirlist = sp.array([sp.array(self._dhat)])
         self.status = "propagating"
@@ -29,24 +30,30 @@ class Ray:
         return self.dirlist[-1]
 
     def point_append(self, p, k):
+        """
+        Appends the lists of position and direction vectors with the position and the normalised direction vector.
+        """
         mag_k = sp.sqrt(sp.dot(k, k))
-        khat = k/mag_k
-        self.poslist = sp.append(self.poslist, sp.array([p]), axis = 0)
-        self.dirlist = sp.append(self.dirlist, [khat], axis = 0)
+        khat = k / mag_k
+        self.poslist = sp.append(self.poslist, sp.array([p]), axis=0)
+        self.dirlist = sp.append(self.dirlist, [khat], axis=0)
 
     def vertices(self):
         return self.poslist
 
+
 class OpticalElement:
     """
-    Base class for all optical elements. Propagates ray through elements.
+    Base class for all optical elements.
     """
+
     def __init__(self, type):
         self.type = type
 
     def propagate_ray(self, ray):
         """propagate a ray through the optical element"""
         raise NotImplementedError()
+
 
 class SphericalRefraction(OpticalElement):
     """
@@ -58,60 +65,79 @@ class SphericalRefraction(OpticalElement):
         self.curve = curve
         self.n1 = n1
         self.n2 = n2
-        self.aperture = aperture # specifying in y direction
+        self.aperture = aperture  # specifying in y direction
         self.origin = sp.array([0., 0., 0.])
         type = "spherical refractor"
         OpticalElement.__init__(self, type)
 
     def intercept(self, ray):
-        # aperture not implemented
+        """
+        Calculates the intercept of the original ray with the surface. Checks if flat, convex or concave.
+
+        :return: point of intersection
+        """
         if ray.status == "terminated":
             print("The ray has terminated.")
+        # for flat surface
         elif self.curve == 0:
-            # intercept z must be z_0
-            # length of flat surface = aperture
-            ray.l = self.z_0/ray.k()[2]
+            length = self.z_0 / ray.k()[2]
+            plane_int = ray.p() + ray.k() * length
+            return plane_int
+        # for convex surface
         elif self.curve > 0:
-            self.origin = sp.array([0. , 0., self.z_0]) + sp.array([0., 0., (1/self.curve)])
+            self.origin = sp.array([0., 0., self.z_0]) + sp.array([0., 0., (1 / self.curve)])
             op = self.origin - ray.p()
+            # find projection of vector between ray and sphere origins onto ray direction
             ray_len = sp.dot(ray.k(), op)
-            disc = ray_len**2 - (sp.dot(op, op) - (1/self.curve)**2)
+            # calculate discriminant of solution to quadratic
+            disc = ray_len ** 2 - (sp.dot(op, op) - (1 / self.curve) ** 2)
             if disc <= 0:
                 print("Imaginary solution.")
                 ray.status = "terminated"
             else:
                 root = sp.sqrt(disc)
-                l = ray_len - abs(root)
-                sph_int = ray.p() + ray.k() * l
+                # carry out rest of solution
+                length = ray_len - abs(root)
+                sph_int = ray.p() + ray.k() * length
                 return sph_int
+        # for concave surface
         elif self.curve < 0:
-            self.origin = sp.array([0. , 0., self.z_0]) - sp.array([0., 0., abs(1/self.curve)])
+            self.origin = sp.array([0., 0., self.z_0]) - sp.array([0., 0., abs(1 / self.curve)])
             op = self.origin - ray.p()
             ray_len = sp.dot(ray.k(), op)
-            disc = ray_len**2 - (sp.dot(op, op) - (1/self.curve)**2)
+            disc = ray_len ** 2 - (sp.dot(op, op) - (1 / self.curve) ** 2)
             if disc <= 0:
                 print("Imaginary solution.")
                 ray.status = "terminated"
             else:
                 root = sp.sqrt(disc)
-                l = ray_len + abs(root)
-                sph_int = ray.p() + ray.k() * l
+                length = ray_len + abs(root)
+                sph_int = ray.p() + ray.k() * length
                 return sph_int
         else:
             print("Ray intercept not able to be found.")
             ray.status = "terminated"
 
     def snell(self, ray):
-        if (sp.sin(self.n1/self.n2))**2 > 1:
+        """
+        Carries out Snell's law.
+
+        :return: new direction of a ray vector after undergoing snell's law
+        """
+        # prevent total internal reflection
+        if (sp.sin(self.n1 / self.n2)) ** 2 > 1:
             print("Ray could not be refracted.")
             ray.status = "terminated"
         elif ray.status == "terminated":
             print("Ray is no longer propagating.")
         else:
+            # find normal for a spherical surface - needs to be corrected for a plane
             normal = self.intercept(ray) - self.origin
             norm_hat = normal / sp.sqrt(sp.dot(normal, normal))
-            root = sp.sqrt(1 - (self.n1/self.n2)**2 * (1 - sp.dot(ray.k(), norm_hat)))
-            new_direc = (self.n1/self.n2) * ray.k() + ((self.n1/self.n2) * sp.dot(ray.k(), norm_hat) - root) * norm_hat
+            # carry out vector form of snell's law
+            root = sp.sqrt(1 - (self.n1 / self.n2) ** 2 * (1 - sp.dot(ray.k(), norm_hat)))
+            new_direc = (self.n1 / self.n2) * ray.k() + (
+                        (self.n1 / self.n2) * sp.dot(ray.k(), norm_hat) - root) * norm_hat
             return new_direc
 
     def propagate_ray(self, ray):
@@ -119,20 +145,32 @@ class SphericalRefraction(OpticalElement):
         newdirec = self.snell(ray)
         if ray.status == "terminated":
             print("Ray is no longer propagating.")
+        # test for ray not intercepting due to aperture
+        elif intercept[1] > self.aperture:
+            print("Ray does not intercept surface.")
         else:
             ray.point_append(intercept, newdirec)
 
     def paraxial_focus(self):
-        r1 = Ray(pos = [0., 0.1, 0.], direc = [0., 0., 1.])
+        """
+        Calculates paraxial focus by propogating rays close to the optical axis and finding their intercept.
+
+        :return: predicted paraxial focus of the surface
+        """
+        r1 = Ray(pos=[0., 0.1, 0.], direc=[0., 0., 1.])
         self.propagate_ray(r1)
-        dist = -r1.p()[1]/r1.k()[1]
+        dist = -r1.p()[1] / r1.k()[1]
         focus = r1.p()[2] + dist * r1.k()[2]
         # expected = self.n2 / (self.curve * (self.n2 - self.n1))
         # print(expected)
-        print("The paraxial focus is: " + str(focus))
+        print("The paraxial focus is: " + str(focus) + "mm")
         return focus
 
+
 class OutputPlane(OpticalElement):
+    """
+    A plane with which the rays intersect to plot their final position.
+    """
 
     def __init__(self, z_1):
         type = "output plane"
@@ -143,7 +181,8 @@ class OutputPlane(OpticalElement):
         if ray.status == "terminated":
             print("Ray is no longer propagating.")
         else:
-            ray.l2 = (self.z_1 - ray.p()[2])/ray.k()[2]
+            # intercept calculated similarly to as with spherical surface
+            ray.l2 = (self.z_1 - ray.p()[2]) / ray.k()[2]
             return ray.p() + ray.l2 * ray.k()
 
     def propagate_ray(self, ray):
